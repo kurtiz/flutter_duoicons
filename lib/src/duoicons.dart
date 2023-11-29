336a492074f4047aab7b47c7652f728e92a55d68
Aaron Will Djaba <papiliocurtis@gmail.com>
2023-11-29 04:24:05 +0000

    added first file

---
+++ lib/src/duoicons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DuoIcons extends StatelessWidget {
+  const DuoIcons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
